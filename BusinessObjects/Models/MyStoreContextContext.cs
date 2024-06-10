afadbc9f982985d31972aa9da4332dd6912b60e2
hailt <hailt@HAILE>
2024-06-10 20:55:17 +0700

    Add project files.

---
+++ BusinessObjects/Models/MyStoreContextContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessObjects.Models;
+
+public partial class MyStoreContextContext : DbContext
+{
+    public MyStoreContextContext()
+    {
+    }
+
+    public MyStoreContextContext(DbContextOptions<MyStoreContextContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AccountMember> AccountMembers { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=HAILE\\HAILN;Database=MyStoreContext;User Id=sa;Password=123;Encrypt=False;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AccountMember>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__AccountM__0CF04B3822E30419");
+
+            entity.ToTable("AccountMember");
+
+            entity.HasIndex(e => e.EmailAddress, "UQ__AccountM__49A1474048060905").IsUnique();
+
+            entity.Property(e => e.MemberId)
+                .HasMaxLength(20)
+                .HasColumnName("MemberID");
+            entity.Property(e => e.EmailAddress).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(80);
+            entity.Property(e => e.MemberPassword).HasMaxLength(80);
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A2B29D78B69");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName).HasMaxLength(15);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6ED75950CBC");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.ProductName).HasMaxLength(40);
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Products__Catego__29572725");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DataAccessLayer/ProductDAO.cs
@@ -0,0 +1,107 @@
+﻿using BusinessObjects;
+using BusinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ProductDAO
+    {
+        private static List<Product> listProducts = GetProducts();
+        public ProductDAO()
+        {
+            Product chai = new Product(1, "Chai", 3, 12, 18);
+            Product chang = new Product(2, "chang", 1, 23, 19);
+            Product aniseed = new Product(3, "Aniseed Syrup", 2, 23, 10);
+            listProducts = new List<Product> { chai, chang, aniseed };
+        }
+        public List<Product> GetProduct()
+        {
+            return listProducts;
+        }
+
+        public static List<Product> GetProducts()
+        {
+            var listProducts = new List<Product>();
+            try
+            {
+                using var db = new MyStoreContextContext();
+                listProducts = db.Products.ToList();
+           
+            }
+            catch (Exception ex) { }
+            return listProducts;
+        }
+
+        public static void SaveProduct(Product p)
+        {
+            try
+            {
+                using var db = new MyStoreContextContext();
+                db.Products.Add(p);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void UpdateProduct(Product product)
+        {
+            using var db = new MyStoreContextContext();
+            foreach (var p in listProducts.ToList())
+            {
+                if (p.ProductId == product.ProductId)
+                {
+                    p.ProductId = product.ProductId;
+                    p.ProductName = product.ProductName;
+                    p.UnitPrice = product.UnitPrice;
+                    p.UnitsInStock = product.UnitsInStock;
+                    p.CategoryId = product.CategoryId;
+                    db.Products.Update(p);
+                    db.SaveChanges();
+                }
+                
+
+            }
+            
+
+        }
+
+        public static void DeleteProduct(Product product)
+        {
+            using var db = new MyStoreContextContext();
+            foreach (Product p in listProducts.ToList())
+            {
+                if (p.ProductId == product.ProductId)
+                {
+                    db.Products.Remove(product);
+                    db.SaveChanges();
+                }
+               
+            }
+           
+
+
+        }
+
+        public static Product GetProductById(int id)
+        {
+            try
+            {
+                using var db = new MyStoreContextContext();
+                return db.Products.FirstOrDefault(p => p.ProductId == id);
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+
+    }
+}
